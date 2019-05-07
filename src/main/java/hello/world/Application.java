1573a17bd2010ae21f166b1074c731c287f6ef42
Rodrigo Santos <rodrigo.santos@m2msolutions.com.br>
2019-05-07 20:12:54 -0300

    continuando estudo

--- src/main/java/hello/world/Application.java
+++ src/main/java/hello/world/Application.java
@@ -1,13 +1,15 @@
 package hello.world;
 
 import hello.world.model.Vehicle;
+import hello.world.model.WeatherService;
 import io.micronaut.context.ApplicationContext;
 import io.micronaut.context.BeanContext;
 import io.micronaut.runtime.Micronaut;
+import io.micronaut.runtime.context.scope.refresh.RefreshEvent;
 
 public class Application {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         Micronaut.run(Application.class);
 
         /*Vehicle vehicle = BeanContext.run().getBean(Vehicle.class);
@@ -16,6 +18,25 @@ public class Application {
         try(ApplicationContext context = ApplicationContext.run()){
             Vehicle vehicle = context.getBean(Vehicle.class);
             System.out.println(vehicle.start());
+
+            WeatherService weatherService = context.getBean(WeatherService.class);
+
+            System.out.println(weatherService.latestForecast());
+
+            Thread.sleep(2000);
+
+            weatherService = context.getBean(WeatherService.class);
+
+            System.out.println(weatherService.latestForecast());
+
+            context.publishEvent(new RefreshEvent());
+
+            Thread.sleep(2000);
+
+            weatherService = context.getBean(WeatherService.class);
+
+            System.out.println(weatherService.latestForecast());
+
         }
     }
 }
